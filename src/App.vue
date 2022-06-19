4585c758db61624124709dee92942b76fb20cb1a
Sherry Tung <sherry.tong@sanganesha.com>
2022-06-19 14:24:35 +0800

    make it better

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Papa from "papaparse";
 import axios from "axios";
 let blockValues = ref([1, 2, 3]);
 const activeCode = ref();
-const loading = ref(true)
+const loading = ref(true);
 const error = ref(false);
 
 axios
@@ -22,15 +22,22 @@ axios
     });
 
     blockValues.value.sort((start, next) => {
-      const order = [49, 50, 51, 52, 81, 87, 69, 82, 65, 83, 68, 70, 90, 88, 67, 86];
+      const order = [
+        49, 50, 51, 52, 81, 87, 69, 82, 65, 83, 68, 70, 90, 88, 67, 86,
+      ];
       return order.indexOf(start.keyCode) - order.indexOf(next.keyCode);
     });
 
-    loading.value = false
+    loading.value = false;
   });
 
 const clearValues = () => {
   blockValues.value.forEach((i) => (i.result = null));
+};
+
+const waiting = ref(true);
+const cancelWaiting = () => {
+  waiting.value = false;
 };
 
 onBeforeMount(() => {
@@ -46,6 +53,11 @@ onBeforeMount(() => {
     }
 
     // // space
+    if (e.keyCode === 32) {
+      waiting.value = true;
+      clearValues();
+      return;
+    }
     // if (e.keyCode === 32) {
     //   error.value = true;
     //   setTimeout(() => {
@@ -56,20 +68,25 @@ onBeforeMount(() => {
 
     // enter
     if (e.keyCode === 13) {
+      if (waiting.value) {
+        cancelWaiting();
+        return;
+      }
       clearValues();
       return;
     }
-    
 
-    const activeIndex = blockValues.value.findIndex((i) => i.keyCode === e.keyCode);
+    const activeIndex = blockValues.value.findIndex(
+      (i) => i.keyCode === e.keyCode
+    );
 
-    if ((event.shiftKey) && (activeIndex!== -1)) {
+    if (event.shiftKey && activeIndex !== -1) {
       activeCode.value = activeIndex;
       blockValues.value[activeIndex].result = false;
       setTimeout(() => {
         blockValues.value[activeIndex].result = null;
       }, 2000);
-      return
+      return;
     }
     if (activeIndex !== -1) {
       activeCode.value = activeIndex;
@@ -77,41 +94,28 @@ onBeforeMount(() => {
     }
   });
 });
-
-// const res = Papa.parse("./questions.csv", {
-//   download: true,
-//   complete: (results) => {
-//     const datas = results.data;
-//     datas.shift(0);
-//     const trueCodes = [0, "O", "o", "Ｏ", "ｏ"];
-//     const falseCodes = ["x", "X", "Ｘ", "ｘ"];
-//     blockValues.value = datas.map((i) => {
-//       return {
-//         question: i[0],
-//         ans: trueCodes.includes(i[1]) ? 79 : 88,
-//         result: null,
-//       };
-//     });
-//   },
-// });
 </script>
 
 <template>
-  
   <div v-if="loading" class="wrong-container">
-    <img src="./assets/loading.gif" />
+    <img src="./assets/loading.gif" alt="" />
   </div>
-  <div v-else class="container">
+  <div v-if="waiting" class="welcome">
+    <img src="./assets/banner.png" alt="" />
+  </div>
+  <div v-if="!loading && !waiting" class="container">
     <div
       class="block"
-      :class="item.result === true ? 'correct' : item.result === false ? 'wrong' : ''"
+      :class="
+        item.result === true ? 'correct' : item.result === false ? 'wrong' : ''
+      "
       v-for="(item, index) in blockValues"
       :key="`${item.answer}-${index}`"
     >
       <h1 v-html="item.answer"></h1>
       <div v-show="item.result === false" class="wrong-container">
-      <img class="wrong" src="./assets/wrong.png" />
-    </div>
+        <img class="wrong" src="./assets/wrong.png" />
+      </div>
     </div>
   </div>
 </template>
@@ -130,23 +134,31 @@ onBeforeMount(() => {
 }
 
 body {
-  background: url('assets/bg.jpg'); /* fallback for old browsers */
+  background: url("assets/bg.png"); /* fallback for old browsers */
+  background-size: cover;
+  // width: 100%;
   height: 100vh;
+  background-position: center;
+  background-repeat: no-repeat;
   margin: 0;
 }
 
 .container {
   margin: auto;
-  width: 80vw;
+  width: 90vw;
   height: auto;
   display: grid;
-  grid-template-columns: repeat(4, 120px);
-  grid-template-rows: repeat(4, 120px);
+  grid-template-columns: repeat(4, minmax(max-content, 300px));
+  grid-template-rows: repeat(4, minmax(max-content, 180px));
+  // grid-template-rows: repeat(4, 300px);
+  gap: 8px;
   justify-content: center;
 }
 .block {
-  background: #ffe24a;
-  border: 1px solid #fff;
+  background: url("assets/block.png");
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -160,32 +172,30 @@ body {
     position: relative;
     &::before {
       position: absolute;
-      top: 6px;
       content: "";
-      border: 4px solid #f14343;
-      border-radius: 100%;
-      width: 100px;
-      height: 100px;
+      background: url("./assets/border.png");
+      background-size: contain;
+      background-position: center;
+      background-repeat: no-repeat;
+      width: 100%;
+      height: 100%;
     }
-  }
-  &.wrong {
-    background: #d33e3e;
   }
   h1 {
     margin: 0;
-    font-size: 16px;
+    font-size: 36px;
+    color: #593328;
   }
 }
-
 
 .wrong-container {
   position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -50%) scale(0.3);
+  transform: translate(-50%, -50%);
   z-index: 100;
   .wrong {
-    width: 300px;
+    width: 100%;
     animation: rubberBand 0.8s;
     animation-iteration-count: 3;
   }
@@ -208,5 +218,33 @@ body {
 .rubberBand {
   -webkit-animation-name: rubberBand;
   animation-name: rubberBand;
+}
+
+.welcome {
+  background: url("assets/welcome.jpg");
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  img {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 50%;
+    top: 50%;
+    animation: disappear 1.5s infinite;
+  }
+}
+@keyframes disappear {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 </style>
